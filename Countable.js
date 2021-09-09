1e957799024a0d126a974f62643bd6f5151d361b
Kane <kane@contentharmony.com>
2021-09-09 16:25:19 -0700

    Adding line breaks as sentences.

--- Countable.js
+++ Countable.js
@@ -146,7 +146,7 @@
 
     return {
       paragraphs: trimmed ? (trimmed.match(options.hardReturns ? /\n{2,}/g : /\n+/g) || []).length + 1 : 0,
-      sentences: trimmed ? (trimmed.match(/[.?!…]+./g) || []).length + 1 : 0,
+      sentences: trimmed ? (trimmed.match(/[.?!…\n]+./g) || []).length + 1 : 0,
       words: trimmed ? (trimmed.replace(/['";:,.?¿\-!¡]+/g, '').match(/\S+/g) || []).length : 0,
       characters: trimmed ? decode(trimmed.replace(/\s/g, '')).length : 0,
       all: decode(original).length
